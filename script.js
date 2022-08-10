fc17e3014804e75c21449448a2595e747fdbc572
Raga <ragawilliam570@gmail.com>
2022-08-10 22:03:34 +0300

    Did some changes on the burger menu

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
 
- // typing text animation script
+//  typing text animation script
  var typed = new Typed(".typing", {
     strings: ["Client", "Model"],
     typeSpeed: 100,
